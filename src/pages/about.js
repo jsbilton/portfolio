3f084c0b4c64d8fde8df90e0df186c4f82dfcf08
Jared Bilton <jsbilton@gmail.com>
2017-01-30 16:54:01 -0500

    added-home-&-about

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+const React = require('react')
+const About = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>About Me</h1>
+      </div>
+   )
+  }
+})
+module.exports = About
